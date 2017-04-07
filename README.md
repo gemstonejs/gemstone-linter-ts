2b267e8b25ce4d608f94f60a80e57e38844049f0
Ralf S. Engelschall <rse@engelschall.com>
2017-04-08 00:49:33 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# gemstone-linter-ts
-Linter for TypeScript
+
+<img src="https://rawgit.com/gemstonejs/gemstone-artwork/master/gemstone-logo-white.svg" width="300" align="right" alt=""/>
+
+Gemstone Linter TypeScript
+==========================
+
+<p/>
+<img src="https://nodei.co/npm/gemstone-linter-ts.png?downloads=true&stars=true" alt=""/>
+<p/>
+<img src="https://david-dm.org/rse/gemstone-linter-ts.png" alt=""/>
+
+About
+-----
+
+This is the Linter for TypeScript files in the
+[Gemstone JavaScript Technology Stack](http://gemstonejs.com).
+It is based on [TSLint](https://palantir.github.io/tslint/).
+
+Copyright &amp; License
+-----------------------
+
+Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
+
---
+++ gemstone-linter-ts.js
@@ -0,0 +1,58 @@
+/*
+**  GemstoneJS -- Gemstone JavaScript Technology Stack
+**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+*/
+
+/*  load external requirements  */
+const fs        = require("fs-promise")
+const path      = require("path")
+const TSLint    = require("tslint")
+
+/*  exported API function  */
+module.exports = async function (filenames, opts = {}, report = { sources: {}, findings: [] }) {
+    /*  setup TSLint CLI engine  */
+    let rules = {}
+    Object.assign(rules, opts.rules)
+    let linter = new TSLint.Linter({})
+    let linterConfigFile = require.resolve("gemstone-config-tslint/tslint.json")
+
+    /*  interate over all source files  */
+    let passed = true
+    if (typeof opts.progress === "function")
+        opts.progress(0.0, "linting TS: starting")
+    for (let i = 0; i < filenames.length; i++) {
+        /*  indicate progress  */
+        if (typeof opts.progress === "function")
+            opts.progress(i / filenames.length, `linting TS: ${filenames[i]}`)
+
+        /*  execute TSLint on given source file  */
+        let content = await fs.readFile(filenames[i], "utf8")
+        let configuration = TSLint.Configuration.findConfiguration(linterConfigFile, filenames[i]).results
+        linter.lint(filenames[i], content, configuration)
+        let result = linter.getResult()
+
+        /*  report linting results  */
+        if (result.errorCount > 0 || result.warningCount > 0) {
+            passed = false
+            result.failures.forEach((failure) => {
+                let filename = path.relative(process.cwd(), failure.fileName)
+                report.sources[filename] = failure.rawLines
+                let [ ruleProc, ruleId ] = [ "tslint", failure.ruleName ]
+                report.findings.push({
+                    ctx:      "TS",
+                    filename: filename,
+                    line:     failure.startPosition.lineAndCharacter.line + 1,
+                    column:   failure.startPosition.lineAndCharacter.character + 1,
+                    message:  failure.failure,
+                    ruleProc: ruleProc,
+                    ruleId:   ruleId
+                })
+            })
+        }
+    }
+    if (typeof opts.progress === "function")
+        opts.progress(1.0, "")
+    return passed
+}
+
